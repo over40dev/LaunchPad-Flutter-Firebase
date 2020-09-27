7ef1d2bc871f74cdbea52d834c9c3130f3c97de9
Cogentx360 Labs <cogentx@Seans-MacBook-Pro.local>
2020-09-27 16:30:37 -0230

    Initial Flutter|Dart App for mobile with ListView

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,7 @@ class LaunchPadApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to this Flutter Firebase Starter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: LaunchPadHomePage(),
-    );
-  }
-}
-
-class LaunchPadHomePage extends StatefulWidget {
-  @override
-  _LaunchPadHomePageState createState() => _LaunchPadHomePageState();
-}
-
-class _LaunchPadHomePageState extends State<LaunchPadHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('LaunchPad Flutter Firebase'),
-      ),
-      body: Center(
-        child: RandomWords(),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -45,9 +22,40 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LaunchPad Flutter Firebase'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
